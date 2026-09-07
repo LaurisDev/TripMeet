636469fe8c75324305f2a269167f71f8eb1209cf
Laura Bedoya <laurabedoyacoronado@gmail.com>
2026-09-06 19:56:24 -0500

    diseño de interfaz

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'registro_screen.dart'; // ajusta la ruta si está en una subcarpeta
+import 'registro_screen.dart';
+import 'app_theme.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,8 +14,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: RegistroScreen(),
+    return MaterialApp(
+      theme: AppTheme.lightTheme, 
+      home: const RegistroScreen(),
     );
   }
 }
